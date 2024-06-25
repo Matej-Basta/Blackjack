449d540e870c09ecbc0a5b846eec4f5cb8af5d52
matej <matej.basta@email.cz>
2024-06-25 12:29:17 +0200

    shuffling and dealing

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import random
+
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
 ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 for suit in suits:
     for rank in ranks:
-        print([suit, rank])
+        cards.append([suit, rank])
+
+def shuffle():
+    random.shuffle(cards)
+
+def deal(num_of_cards = 1):
+    dealt_cards = []
+    for i in range(num_of_cards): 
+        dealt_cards.append(cards.pop())
+
+    return dealt_cards
+
+shuffle()
