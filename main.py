bd2cfe9d60a9670f45c0925541ce2a8a21c53fe2
matej <matej.basta@email.cz>
2024-06-25 13:05:00 +0200

    Deck class

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import random
 
-cards = []
-suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "clubs", "hearts", "diamonds"]
+        ranks = [
+            {"rank": "A", "value": 11},
+            {"rank": "2", "value": 2},
+            {"rank": "3", "value": 3},
+            {"rank": "4", "value": 4},
+            {"rank": "5", "value": 5},
+            {"rank": "6", "value": 6},
+            {"rank": "7", "value": 7},
+            {"rank": "8", "value": 8},
+            {"rank": "9", "value": 9},
+            {"rank": "10", "value": 10},
+            {"rank": "J", "value": 10},
+            {"rank": "Q", "value": 10},
+            {"rank": "K", "value": 10},
+        ]
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append([suit, rank])
 
-def shuffle():
-    random.shuffle(cards)
+    def shuffle(self):
+        random.shuffle(self.cards)
 
-def deal(num_of_cards = 1):
-    dealt_cards = []
-    for i in range(num_of_cards): 
-        dealt_cards.append(cards.pop())
+    def deal(self, num_of_cards = 1):
+        dealt_cards = []
+        for i in range(num_of_cards): 
+            dealt_cards.append(self.cards.pop())
 
-    return dealt_cards
+        return dealt_cards
 
-shuffle()
+deck1 = Deck()
+deck2 = Deck()
+deck2.shuffle()
+print(deck2.cards)
